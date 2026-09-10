3a0ce99627848b6d8a29521bf74aff303337912c
jjean-v <14jean04@gmail.com>
2026-09-10 13:15:35 +0200

    dev: method for testing proxyConnected

--- dbus.js
+++ dbus.js
@@ -1,31 +1,44 @@
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 
-const proxy = new Gio.DBusProxy({
-    g_connection: Gio.DBus.session,
-    g_name: 'org.mpris.MediaPlayer2.deezer',
-    g_object_path: '/org/mpris/MediaPlayer2',
-    g_interface_name: 'org.mpris.MediaPlayer2.Player',
-    g_flags: Gio.DBusProxyFlags.NONE,
-});
-proxy.init(null);
 
-// Lecture du titre courant
-function currentTitle() {
-    const meta = proxy.get_cached_property('Metadata');
-    if (!meta) return null;
-    const dict = meta.recursiveUnpack();   // -> objet JS normal
-    return dict['xesam:title'];
+class DeezerController {
+    constructor() {
+        this.proxy = new Gio.DBusProxy({
+            g_connection: Gio.DBus.session,
+            g_name: 'org.mpris.MediaPlayer2.deezer',
+            g_object_path: '/org/mpris/MediaPlayer2',
+            g_interface_name: 'org.mpris.MediaPlayer2.Player',
+            g_flags: Gio.DBusProxyFlags.NONE,
+        });
+        this.proxy.init(null);
+    }
+
+    get currentTitle() {
+        const meta = this.proxy.get_cached_property('Metadata');
+        if (!meta) return null;
+        const dict = meta.recursiveUnpack();   // -> objet JS normal
+        return dict['xesam:title'];
+    }
+
+    // method to keep the proxy connected, only used of testing
+    proxyConnected() {
+
+        // Affichage immédiat au lancement
+        console.log('Titre courant : ' + this.currentTitle);
+
+        // Mise à jour automatique à chaque changement
+        this.proxy.connect('g-properties-changed', () => {
+            console.log('Changement -> ' + this.currentTitle);
+        });
+    }
 }
+    
 
-// Affichage immédiat au lancement
-console.log('Titre courant : ' + currentTitle());
+const deezer = new DeezerController();
 
-// Mise à jour automatique à chaque changement
-proxy.connect('g-properties-changed', () => {
-    console.log('Changement -> ' + currentTitle());
-});
 
+deezer.proxyConnected();
 // Boucle d'événements : garde le programme en vie pour écouter les signaux
 const loop = new GLib.MainLoop(null, false);
 loop.run();
