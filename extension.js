0c135f861f5cde28fee45e0ec90b631a7f73f81d
jjean-v <14jean04@gmail.com>
2026-09-12 08:10:22 +0200

    dev: play and next button

--- extension.js
+++ extension.js
@@ -79,6 +79,51 @@ class Pause extends St.Icon {
     }
 });
 
+const Play = GObject.registerClass(
+class Play extends St.Icon {
+    _init() {
+        super._init({
+            track_hover: true,
+            can_focus: true,
+            reactive: true,
+            icon_name: play,
+            style_class: 'system-status-icon',
+        });
+
+        // Listen for update of left padding in settings
+        this.connect('button-press-event', () => {
+            //spotify.previous();
+            console.log("\n\n\n\nThe song is on play\n\n\n\n");
+        });
+    }
+
+    _styleChanged() {
+        this.set_style(styleStr('left', 'prev'));
+    }
+});
+
+const Next = GObject.registerClass(
+class Next extends St.Icon {
+    _init() {
+        super._init({
+            track_hover: true,
+            can_focus: true,
+            reactive: true,
+            icon_name: forward,
+            style_class: 'system-status-icon',
+        });
+
+        // Listen for update of left padding in settings
+        this.connect('button-press-event', () => {
+            //spotify.previous();
+            console.log("\n\n\n\nListen to next song\n\n\n\n");
+        });
+    }
+
+    _styleChanged() {
+        this.set_style(styleStr('left', 'prev'));
+    }
+});
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
@@ -88,6 +133,7 @@ class Indicator extends PanelMenu.Button {
         this.bar = new St.BoxLayout();
         this.bar.add_child(new Previous());
         this.bar.add_child(new Pause());
+        this.bar.add_child(new Next());
 
         this.add_child(this.bar);
         const deezer = new DeezerController();
